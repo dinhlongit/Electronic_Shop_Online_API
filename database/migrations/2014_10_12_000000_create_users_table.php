1104f2ba3ec28af6c76169da6f72367ff102461f
dinhlongit <zingnewtop@gmail.com>
2020-05-02 20:57:40 +0800

    begin_db

---
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -0,0 +1,43 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateUsersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('users', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('first_name');
+            $table->string('last_name');
+            $table->string('phone_number')->nullable();
+            $table->string('email')->unique();
+            $table->timestamp('email_verified_at')->nullable();
+            $table->string('address')->nullable();
+            $table->string('password');
+            $table->string('profile_photo');
+            $table->date("birthday");
+            $table->string("address_id");
+            $table->rememberToken();
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('users');
+    }
+}
