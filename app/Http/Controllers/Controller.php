569ba05588d5bf7be84fea31a018bc20e40a1181
dinhlongit <zingnewtop@gmail.com>
2020-06-17 10:19:01 +0800

    fix loi api

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -23,7 +23,7 @@ use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
  *     ),
  *         @OA\Server(
  *         description="OpenApi server",
- *         url="/public"
+ *         url=""
  *     ),
  *     @OA\ExternalDocumentation(
  *         description="Find out more about Swagger",
