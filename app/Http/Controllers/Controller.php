e0f88d9460b026f154e68005abfa4f92913a6653
dinhlongit <zingnewtop@gmail.com>
2020-06-17 10:20:01 +0800

    loi duong dan

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -23,7 +23,7 @@ use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
  *     ),
  *         @OA\Server(
  *         description="OpenApi server",
- *         url=""
+ *         url="/public"
  *     ),
  *     @OA\ExternalDocumentation(
  *         description="Find out more about Swagger",
