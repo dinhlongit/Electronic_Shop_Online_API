483c5d1fda89e80755870e2d7293345998073a6d
dinhlongit <zingnewtop@gmail.com>
2020-06-24 16:41:15 +0800

    fix update user

--- app/Repositories/Report/ReportEloquentRepository.php
+++ app/Repositories/Report/ReportEloquentRepository.php
@@ -13,17 +13,7 @@ class ReportEloquentRepository implements ReportRepositoryInterface
 {
     public function getTranSactionStatus()
     {
-        return DB::table('transactions as t')
-            ->leftJoin('users','t.user_id','=','users.id')
-            ->leftJoin('transaction_products','t.id','=','transaction_products.transaction_id')
-            ->leftJoin('addresses','t.address_id','=','addresses.id')
-            ->leftJoin('transaction_statuses','t.status_id','=','transaction_statuses.id')
-            ->select('t.status_id','t.user_id','t.full_name',
-                't.street','addresses.name as city','users.phone_number','users.name',
-                DB::raw('SUM(transaction_products.price * transaction_products.amount) as total')
-                , 'transaction_statuses.name as status')
-            ->groupBy('t.status_id')
-            ->get();
+      
     }
 
 
